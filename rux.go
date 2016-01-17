5578004c15b5af0ac36c5df6b1989aeb9811ec97
craig <craig@freightfinch>
2016-01-17 09:49:32 -0800

    wip

--- rux.go
+++ rux.go
@@ -5,36 +5,70 @@ import (
 	"strings"
 )
 
+var tree *Tree
+
 type handler func(w http.ResponseWriter, req *http.Request)
 
 func New() *Tree {
-	return &Tree{
-		root: newNode(),
+	tree = &Tree{
+		methods: map[string]*node{
+			"GET":    newNode(),
+			"POST":   newNode(),
+			"PUT":    newNode(),
+			"DELETE": newNode(),
+			"PATCH":  newNode(),
+		},
 	}
+	return tree
 }
 
 type Tree struct {
-	root *node
+	methods map[string]*node
 }
 
 func (t *Tree) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	t.root.handle(strings.Split(strings.Trim(r.URL.Path, "/"), "/"), w, r)
+	n, ok := t.methods[r.Method]
+	if !ok {
+		notFound(w)
+	} else {
+		n.handle(strings.Split(strings.Trim(r.URL.Path, "/"), "/"), w, r)
+	}
+}
+
+func Vars(r *http.Request) map[string]string {
+	pth := strings.Split(strings.Trim(r.URL.Path, "/"), "/")
+	m := map[string]string{}
+	tree.methods[r.Method].vars(pth, m)
+	return m
 }
 
 func (t *Tree) Get(pth string, h handler) *node {
-	return t.root.add(strings.Split(strings.Trim(pth, "/"), "/"), h)
+	return t.method("GET", pth, h)
 }
 
-func (t *Tree) Post(pth string, h handler) {
+func (t *Tree) Post(pth string, h handler) *node {
+	return t.method("POST", pth, h)
 }
 
-func (t *Tree) Put(pth string, h handler) {
+func (t *Tree) Put(pth string, h handler) *node {
+	return t.method("PUT", pth, h)
 }
 
-func (t *Tree) Delete(pth string, h handler) {
+func (t *Tree) Delete(pth string, h handler) *node {
+	return t.method("DELETE", pth, h)
+}
+
+func (t *Tree) Path(pth string, h handler) *node {
+	return t.method("PATCH", pth, h)
+}
+
+func (t *Tree) method(name, pth string, h handler) *node {
+	return t.methods[name].add(strings.Split(strings.Trim(pth, "/"), "/"), h)
 }
 
 type node struct {
+	id       string
+	key      string
 	handler  handler
 	resource bool
 	children map[string]*node
@@ -46,12 +80,39 @@ func newNode() *node {
 	}
 }
 
+func (n *node) vars(pth []string, m map[string]string) {
+	if len(pth) == 0 && !n.resource {
+		return
+	}
+
+	var c *node
+	var ok bool
+	l := len(pth)
+	if n.resource && l != 0 {
+		m[n.key] = pth[0]
+		c, ok = n.children[n.id]
+	} else if l != 0 {
+		c, ok = n.children[pth[0]]
+	}
+
+	if ok && l != 0 {
+		c.vars(pth[1:], m)
+	}
+}
+
 func (n *node) add(pth []string, h handler) *node {
 	if len(pth) == 0 {
 		n.handler = h
 		return n
 	}
-	c, ok := n.children[pth[0]]
+	n.resource, n.id, n.key = n.isResource(pth[0])
+	var x string
+	if n.resource {
+		x = n.id
+	} else {
+		x = pth[0]
+	}
+	c, ok := n.children[x]
 	if !ok {
 		c = newNode()
 		n.children[pth[0]] = c
@@ -59,18 +120,27 @@ func (n *node) add(pth []string, h handler) *node {
 	return c.add(pth[1:], h)
 }
 
-func (n *node) isResource(s string) bool {
-	return strings.Index(s, "{") == 0
+func (n *node) isResource(s string) (bool, string, string) {
+	var id string
+	r := strings.Index(s, "{") == 0
+	if r {
+		id = s
+	}
+	return r, id, strings.Trim(id, "{}")
 }
 
 func (n *node) handle(pth []string, w http.ResponseWriter, r *http.Request) {
-	if len(pth) == 0 || (len(pth) == 1 && n.isResource(pth[0])) {
+	if len(pth) == 0 {
 		n.handler(w, r)
 		return
 	}
+	var x string
 	if n.resource {
-		n.handle(pth[1:], w, r)
-	} else if c, ok := n.children[pth[0]]; ok {
+		x = n.id
+	} else {
+		x = pth[0]
+	}
+	if c, ok := n.children[x]; ok {
 		c.handle(pth[1:], w, r)
 	} else {
 		notFound(w)
--- rux_test.go
+++ rux_test.go
@@ -1,6 +1,8 @@
 package rux_test
 
 import (
+	"bytes"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 
@@ -12,23 +14,72 @@ import (
 
 var _ = Describe("Gadgets", func() {
 	var (
-		r   *rux.Tree
-		w   *httptest.ResponseRecorder
-		req *http.Request
+		r      *rux.Tree
+		w      *httptest.ResponseRecorder
+		req    *http.Request
+		method string
+		data   string
 	)
 
 	BeforeEach(func() {
+		method = ""
+		data = ""
 		root := func(ww http.ResponseWriter, rr *http.Request) {
+			method = rr.Method
 			ww.Write([]byte("root"))
 		}
 
 		pals := func(ww http.ResponseWriter, rr *http.Request) {
+			method = rr.Method
 			ww.Write([]byte("pals"))
+		}
+
+		post := func(ww http.ResponseWriter, rr *http.Request) {
+			method = rr.Method
+			d, _ := ioutil.ReadAll(rr.Body)
+			data = string(d)
+		}
+
+		delete := func(ww http.ResponseWriter, rr *http.Request) {
+			method = rr.Method
+		}
+
+		pal := func(ww http.ResponseWriter, rr *http.Request) {
+			method = rr.Method
+			ww.Write([]byte("pal"))
+		}
+
+		pets := func(ww http.ResponseWriter, rr *http.Request) {
+			method = rr.Method
+			ww.Write([]byte("pets"))
+		}
+
+		pet := func(ww http.ResponseWriter, rr *http.Request) {
+			method = rr.Method
+			ww.Write([]byte("pet"))
+		}
+
+		colors := func(ww http.ResponseWriter, rr *http.Request) {
+			method = rr.Method
+			ww.Write([]byte("colors"))
+		}
+
+		color := func(ww http.ResponseWriter, rr *http.Request) {
+			method = rr.Method
+			ww.Write([]byte("color"))
 		}
 
 		r = rux.New()
 		r.Get("/", root)
 		r.Get("/pals", pals)
+		r.Post("/pals", post)
+		r.Get("/pals/{id}", pal)
+		r.Get("/pals/{id}/pets", pets)
+		r.Get("/pals/{id}/pets/{pet}", pet)
+		r.Get("/pals/{id}/colors", colors)
+		r.Post("/pals/{id}/colors", post)
+		r.Get("/pals/{id}/colors/{color}", color)
+		r.Delete("/pals/{id}/colors/{color}", delete)
 
 		w = httptest.NewRecorder()
 	})
@@ -52,5 +103,92 @@ var _ = Describe("Gadgets", func() {
 		r.ServeHTTP(w, req)
 		Expect(w.Code).To(Equal(http.StatusOK))
 		Expect(w.Body.String()).To(Equal("pals"))
+	})
+
+	It("gets the first resource", func() {
+		var err error
+		req, err = http.NewRequest("GET", "/pals/1", nil)
+		Expect(err).To(BeNil())
+		r.ServeHTTP(w, req)
+		Expect(w.Code).To(Equal(http.StatusOK))
+		Expect(w.Body.String()).To(Equal("pal"))
+	})
+
+	It("gets the pets collection", func() {
+		var err error
+		req, err = http.NewRequest("GET", "/pals/1/pets", nil)
+		Expect(err).To(BeNil())
+		r.ServeHTTP(w, req)
+		Expect(w.Code).To(Equal(http.StatusOK))
+		Expect(w.Body.String()).To(Equal("pets"))
+	})
+
+	It("gets a pet resource", func() {
+		var err error
+		req, err = http.NewRequest("GET", "/pals/1/pets/5", nil)
+		Expect(err).To(BeNil())
+		r.ServeHTTP(w, req)
+		Expect(w.Code).To(Equal(http.StatusOK))
+		Expect(w.Body.String()).To(Equal("pet"))
+	})
+
+	It("gets the colors collection", func() {
+		var err error
+		req, err = http.NewRequest("GET", "/pals/1/colors", nil)
+		Expect(err).To(BeNil())
+		r.ServeHTTP(w, req)
+		Expect(w.Code).To(Equal(http.StatusOK))
+		Expect(w.Body.String()).To(Equal("colors"))
+		vars := rux.Vars(req)
+		Expect(vars["id"]).To(Equal("1"))
+	})
+
+	It("gets a colors resource", func() {
+		var err error
+		req, err = http.NewRequest("GET", "/pals/3/colors/red", nil)
+		Expect(err).To(BeNil())
+		r.ServeHTTP(w, req)
+		Expect(w.Code).To(Equal(http.StatusOK))
+		Expect(w.Body.String()).To(Equal("color"))
+		vars := rux.Vars(req)
+		Expect(vars["id"]).To(Equal("3"))
+		Expect(vars["color"]).To(Equal("red"))
+	})
+
+	It("POSTS to pals", func() {
+		var err error
+		buf := bytes.NewBuffer([]byte("stu"))
+		req, err = http.NewRequest("POST", "/pals", buf)
+		Expect(err).To(BeNil())
+		r.ServeHTTP(w, req)
+		Expect(method).To(Equal("POST"))
+		Expect(w.Code).To(Equal(http.StatusOK))
+		Expect(w.Body.String()).To(Equal(""))
+		Expect(data).To(Equal("stu"))
+	})
+
+	It("POSTS to colors", func() {
+		var err error
+		buf := bytes.NewBuffer([]byte("green"))
+		req, err = http.NewRequest("POST", "/pals/55/colors", buf)
+		Expect(err).To(BeNil())
+		r.ServeHTTP(w, req)
+		Expect(method).To(Equal("POST"))
+		Expect(w.Code).To(Equal(http.StatusOK))
+		Expect(w.Body.String()).To(Equal(""))
+		Expect(data).To(Equal("green"))
+	})
+
+	It("DELETES a color", func() {
+		var err error
+		req, err = http.NewRequest("DELETE", "/pals/55/colors/red", nil)
+		Expect(err).To(BeNil())
+		r.ServeHTTP(w, req)
+		Expect(method).To(Equal("DELETE"))
+		Expect(w.Code).To(Equal(http.StatusOK))
+		Expect(w.Body.String()).To(Equal(""))
+		vars := rux.Vars(req)
+		Expect(vars["id"]).To(Equal("55"))
+		Expect(vars["color"]).To(Equal("red"))
 	})
 })
